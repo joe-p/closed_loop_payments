6df985ac316ffe2f26820172918c3f16077e3897
Joe Polny <joepolny@gmail.com>
2026-03-25 21:11:53 -0400

    multi payments test

--- __test__/payments.test.ts
+++ __test__/payments.test.ts
@@ -14,8 +14,11 @@ async function getAlgoBalance(
   return amount;
 }
 
+const PAYMENTS = 50;
+
 describe("Payments", () => {
   let algorand: AlgorandClient;
+  let adminClient: PaymentsAdminClient;
   let userClient: PaymentsUserClient;
   let zeroAlgoSender: SendingAddress;
   let zeroAlgoReceiver: SendingAddress;
@@ -23,12 +26,12 @@ describe("Payments", () => {
   beforeAll(async () => {
     algorand = AlgorandClient.defaultLocalNet();
     const admin = await algorand.account.dispenserFromEnvironment();
-    const adminClient = await PaymentsAdminClient.create({
+    adminClient = await PaymentsAdminClient.create({
       algorand,
       admin,
-      supply: 200n,
-      prefundAccounts: 2n,
-      prefundTransactions: 1n,
+      supply: 1000n,
+      prefundAccounts: BigInt(PAYMENTS * 2 + 2),
+      prefundTransactions: BigInt(PAYMENTS + 1),
     });
     zeroAlgoSender = algorand.account.random();
     zeroAlgoReceiver = algorand.account.random();
@@ -60,5 +63,26 @@ describe("Payments", () => {
 
     expect(await userClient.balance(zeroAlgoSender)).toBe(90n);
     expect(await userClient.balance(zeroAlgoReceiver)).toBe(10n);
+  });
+
+  it(`${PAYMENTS} payments`, async () => {
+    const senders = Array.from({ length: PAYMENTS }, () =>
+      algorand.account.random(),
+    );
+    const receivers = Array.from({ length: PAYMENTS }, () =>
+      algorand.account.random(),
+    );
+
+    for (const sender of senders) {
+      await adminClient.instantiateAccount(sender);
+      await adminClient.addToCirculation(1n, sender);
+    }
+    for (const receiver of receivers) {
+      await adminClient.instantiateAccount(receiver);
+    }
+
+    for (let i = 0; i < PAYMENTS; i++) {
+      await userClient.transfer(senders[i], receivers[i], 1n);
+    }
   });
 });
